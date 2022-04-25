b5d105ce6d30c03e984615bfb6d76571e57e9dba
Drca Vuk <vukdrca@gmail.com>
2022-04-25 21:24:29 +0200

    http hook

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,48 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+import axios from "axios";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(async (url, method = "GET", data, headers = '') => {
+    setIsLoading(true);
+    const httpAbortCtrl = new AbortController();
+    activeHttpRequests.current.push(httpAbortCtrl);
+
+    try {
+      const response = await axios({
+        method,
+        url,
+        data,
+        signal: httpAbortCtrl.signal,
+        headers
+      });
+
+      activeHttpRequests.current = activeHttpRequests.current.filter(
+        (reqCtrl) => reqCtrl !== httpAbortCtrl
+      );
+
+      setIsLoading(false);
+      return response;
+    } catch (err) {
+      setError(err.message);
+      setIsLoading(false);
+      throw err;
+    }
+  }, []);
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
--- src/shop/pages/product/Product.jsx
+++ src/shop/pages/product/Product.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams } from "react-router-dom";
+import Description from "../../../shared/components/UI/Description/Description";
+import Title from "../../../shared/components/UI/Title/Title";
+import LoadingSpinner from "../../../shared/components/UI/LoadingSpinner/LoadingSpinner";
 
 const Product = props => {
+    const [isLoading , setIsLoading ] = useState(true);
     let { id } = useParams();
     return(
         <div className="container-fluid">
             <div className="container">
-                <h1>Product</h1>
-                <p>ID: {id}</p>
+                {!isLoading && <LoadingSpinner />}
+                {isLoading && 
+                <div className="row">
+                    <div className="col-50">
+                        <img src={props.image} alt={props.alt} />
+                    </div>
+                    <div className="col-50">
+                        <Title>{props.title}</Title>
+                        <Description>{props.description}</Description>
+                    </div>
+                </div>}
             </div>
         </div>
     )
